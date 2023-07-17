041c33c174d0821b0074ed8ff33f53b5ca094ae2
William Liu <liuwilliam424@gmail.com>
2023-07-17 18:30:48 -0400

    Made reviews/writing scaleable with JSON

---
+++ scripts/reviews.js
@@ -0,0 +1,46 @@
+article = document.querySelector('article');
+
+const create_entries = function(data) {
+    data.forEach(function (entry) {
+        const entry_label = document.createElement('div');
+        entry_label.classList.add('entry-label');
+
+        let url = "reviews/" + entry['title'] +'.html';
+        url = url.toLowerCase();
+        url = url.replaceAll(" ", "_")
+
+        const a_url = document.createElement('a');
+        a_url.setAttribute('href', url);
+
+        const titles_label = document.createElement('div');
+        titles_label.classList.add('titles-label');
+
+        const title_label = document.createElement('span');
+        title_label.classList.add('title-label');
+        title_label.textContent = entry['title'];
+
+        const subtitle_label = document.createElement('span');
+        subtitle_label.classList.add('subtitle-label');
+        subtitle_label.textContent = entry['subtitle'];
+
+        const date_label = document.createElement('span');
+        date_label.classList.add('date-label');
+        date_label.textContent = entry['date'];
+
+        titles_label.appendChild(title_label);
+        titles_label.appendChild(subtitle_label);
+        a_url.appendChild(titles_label);
+        entry_label.appendChild(a_url);
+        entry_label.appendChild(date_label);
+        article.append(entry_label);
+    })
+}
+
+fetch('reviews/reviews_data.json').then(function (file_data) {
+    file_data.json().then(
+        function (json_data) {
+            data = json_data['data'];
+            create_entries(data);
+        }
+    );
+})
